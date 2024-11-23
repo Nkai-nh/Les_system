79c7d9778e2aa36aa16e90d79d9d0b23d784cd98
Nguyen Tien Manh <nguyenmanh002347@gmail.com>
2024-11-23 21:15:25 +0700

    update

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require('../models/category');
+const Product = require('../models/product');
 const { formatResponse } = require('../utils/responseFormatter');
 
 exports.getAllCategories = async (req, res, next) => {
@@ -6,6 +7,29 @@ exports.getAllCategories = async (req, res, next) => {
         const categories = await Category.findAll();
         res.status(200).json(formatResponse('Categories retrieved successfully', categories));
     } catch (error) {
+        next(error);
+    }
+};
+
+// API để lấy tất cả sản phẩm theo category
+exports.getAllProductsByCategory = async (req, res, next) => {
+    const { categoryId } = req.params;
+
+    console.log('Category ID:', categoryId); // Logging categoryId
+
+    try {
+        const products = await Product.findAll({
+            where: { category_id: categoryId },
+            include: [{ model: Category, attributes: ['name'] }]
+        });
+
+        if (products.length === 0) {
+            return res.status(404).json(formatResponse('Không tìm thấy sản phẩm cho danh mục này.', []));
+        }
+
+        return res.status(200).json(formatResponse('Products retrieved successfully', products));
+    } catch (error) {
+        console.error(error);
         next(error);
     }
 };
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,5 +4,6 @@ const categoryController = require('../controllers/categoryController');
 const router = express.Router();
 
 router.get('/all', categoryController.getAllCategories);
+router.get('/:categoryId', categoryController.getAllProductsByCategory);
 
 module.exports = router;
